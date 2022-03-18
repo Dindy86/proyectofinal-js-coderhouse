2b754a411750dd40cfd5c2c788095e7b189d7d0a
Dindy Pierre <ing.pierredindy@gmail.com>
2022-03-18 05:11:52 +0100

    He modificado contacto.js, contacto.html, _contacto.scss

--- js/contacto.js
+++ js/contacto.js
@@ -1,28 +1,22 @@
-
 $("#formContact").submit(function (e) { 
-  //e.preventDefault();
-  // $("#contenedorLoader").fadeIn(2000);
-  // $("#contenedorLoader").fadeOut(1000, function() {
-  //   $("#notificacionEnvio").fadeIn(2000, function(){
-  //     $("#notificacionEnvio").fadeOut(2000, function(){
-  //       const formulario = document.getElementById("formContact");
-  //       formulario.reset();
-  //     })
-  //   })
-  // });
-});
-
-// $("#formContact").submit(function (e) { 
-//   e.preventDefault();
-//   fetch("https://formsubmit.co/ajax/ing.pierredindy@gmail.com", {
-//     method: "POST",
-//     body: new FormData(e.target)
-//   })
-//   .then(res => res.ok ? res.json(): Promise.reject(res))
-//   .then(json => {
-//     console.log(json)
-//   })
-//   .catch(err => {
-//     console.log(err);
-//   })
-// })
+  e.preventDefault();
+  fetch("https://formsubmit.co/ajax/ing.pierredindy@gmail.com", {
+    method: "POST",
+    body: new FormData(e.target)
+  })
+  .then(res => res.ok ? res.json(): Promise.reject(res))
+  .then(json => {
+    $("#formContact-contenedorLoader").fadeIn(1000);
+      $("#formContact-contenedorLoader").fadeOut(1000, function() {
+      $("#formContact-notificacionEnvio").fadeIn(2000, function() {
+        $("#formContact-notificacionEnvio").fadeOut(2000, function() {
+        const formulario = document.getElementById("formContact");
+        formulario.reset();
+        })
+      })
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  })
+})
