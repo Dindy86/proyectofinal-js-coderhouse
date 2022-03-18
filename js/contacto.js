3eb775019417e7baee5d73d32de3897ff9056396
Dindy Pierre <ing.pierredindy@gmail.com>
2022-03-18 05:29:24 +0100

    modificar contacto.html, contacto.scss, contacto.js

--- js/contacto.js
+++ js/contacto.js
@@ -9,13 +9,14 @@ $("#formContact").submit(function (e) {
   .then(json => {
     $("#formContact-contenedorLoader").fadeIn(1000);
       $("#formContact-contenedorLoader").fadeOut(1000, function() {
-      $("#formContact-notificacionEnvio").fadeIn(2000, function() {
-        $("#formContact-notificacionEnvio").fadeOut(2000, function() {
+      $("#formContact-notificacionEnvio").fadeIn(2000, function(){
+      $("#formContact-notificacionEnvio").fadeOut(2000, function(){
         const formulario = document.getElementById("formContact");
         formulario.reset();
-        })
       })
-    });
+    })
+  });
+    console.log(json)
   })
   .catch(err => {
     console.log(err);
